2a0dbc3de6c2f8768ecde2b30d2dcaf7f9abab1a
retro <142931114+r5tro@users.noreply.github.com>
2025-07-19 22:27:24 +0100

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	images: {
+		remotePatterns: [
+			// For Discord RPC we need to allow all domains
+			{
+				protocol: "https",
+				hostname: "**",
+			},
+		],
+	},
+};
+
+export default nextConfig;
